147f4884ced5ff3b1501f2614a16d510606fe27e
habib610 <habib54562@gmail.com>
2020-12-22 16:59:03 +0600

    basic starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+        <Link to="/" className="brand">cartGear</Link>
+        </div>
+         <div>
+        <Link to="/">Cart</Link>
+        <Link to="/">Sign In</Link>
+         </div>
+
       </header>
+      <main>
+          <div className="row">
+            <h1>Home screen</h1>
+          </div>
+      </main>
+      <footer className="row center ">
+        <p>Copyright &copy {new Date().getFullYear()} Habib. All Rights Reserved</p>
+      </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
